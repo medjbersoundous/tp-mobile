6ba4c5817c43824a513d3efda1c39fb2a7bb2152
medjbersoundous <medjbersoundous@gmail.com>
2024-03-19 12:36:21 +0100

    fix xml

--- app/src/main/java/com/example/tp3/AfficherActivity.java
+++ app/src/main/java/com/example/tp3/AfficherActivity.java
@@ -4,8 +4,9 @@ import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Space;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AfficherActivity extends AppCompatActivity {
@@ -25,20 +26,24 @@ public class AfficherActivity extends AppCompatActivity {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM student", null);
 
-        TextView textView = findViewById(R.id.textView);
+        LinearLayout parentLayout = findViewById(R.id.parentLayout);
 
         if (cursor.moveToFirst()) {
-            StringBuilder stringBuilder = new StringBuilder();
             do {
                 @SuppressLint("Range") String matricule = cursor.getString(cursor.getColumnIndex("matricule"));
                 @SuppressLint("Range") String nom = cursor.getString(cursor.getColumnIndex("nom"));
                 @SuppressLint("Range") String prenom = cursor.getString(cursor.getColumnIndex("prenom"));
-                stringBuilder.append("Matricule: ").append(matricule).append(", Nom: ").append(nom).append(", Prenom: ").append(prenom).append("\n");
-            } while (cursor.moveToNext());
 
-            textView.setText(stringBuilder.toString());
-        } else {
-            Toast.makeText(this, "No students found.", Toast.LENGTH_SHORT).show();
+
+                TextView textView = new TextView(this);
+                textView.setText("Matricule: " + matricule + "\nNom: " + nom + "\nPrenom: " + prenom);
+                textView.setTextSize(20);
+                parentLayout.addView(textView);
+                Space space = new Space(this);
+                space.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 20)); // Adjust the height as needed
+                parentLayout.addView(space);
+
+            } while (cursor.moveToNext());
         }
 
         cursor.close();
